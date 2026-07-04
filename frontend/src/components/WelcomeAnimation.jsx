6f65da639a6372487a018c8b2fbb0db3562df1b0
internvpv <intern.vp1@nuvo.ai>
2026-07-04 16:25:43 +0530

    fix theme background

--- frontend/src/components/WelcomeAnimation.jsx
+++ frontend/src/components/WelcomeAnimation.jsx
@@ -151,8 +151,8 @@ export default function WelcomeAnimation({ onComplete }) {
         >
           <div style={styles.brandIconInline}>
             <svg
-              width="80"
-              height="80"
+              width="120"
+              height="120"
               viewBox="0 0 24 24"
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
